33c9128555f81a522e6d1741ef2cbf0dc7d5cf2f
Charles-Maxime Laureline Panel <WINEGO@iMac-de-Charles-Maxime.local>
2014-09-23 13:35:14 +0800

    added test class

---
+++ CMPTest.h
@@ -0,0 +1,13 @@
+//
+//  CMPTest.h
+//  Git Test
+//
+//  Created by Charles-Maxime Laureline Panel on 23/09/2014.
+//  Copyright (c) 2014 Charles-Maxime Laureline Panel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CMPTest : NSObject
+
+@end
---
+++ CMPTest.m
@@ -0,0 +1,13 @@
+//
+//  CMPTest.m
+//  Git Test
+//
+//  Created by Charles-Maxime Laureline Panel on 23/09/2014.
+//  Copyright (c) 2014 Charles-Maxime Laureline Panel. All rights reserved.
+//
+
+#import "CMPTest.h"
+
+@implementation CMPTest
+
+@end
